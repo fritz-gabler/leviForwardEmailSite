5b8fa713c87d78540f146828ad2130f0c84ea933
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-09-09 07:35:50 +0200

    serve html file in express server

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,17 @@
+const path = require('path')
 const express = require('express');
-
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log("hi");
-  res.send("hi");
+app.use(express.static(path.join(__dirname, 'frontend')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/index.html'))
 })
 
 const forwardCreate = require('./routes/mail_forward/create')
+const getMailAccounts = require('./routes/mail_accounts/get/getMail')
 
 app.use('/mail-forward', forwardCreate);
+app.use('/mail-accounts', getMailAccounts);
 
 app.listen(3000);
