cdb60bb2b335613fddffcb169aab6edd587787e0
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-09-14 12:42:41 +0200

    export app so its uable in other files

--- src/app/index.js
+++ src/app/index.js
@@ -2,16 +2,20 @@ const path = require('path')
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'frontend')))
+app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/index.html'))
+    res.sendFile(path.join(__dirname, 'frontend/index.html'));
 })
 
 const forwardCreate = require('./routes/mail_forward/create')
 const getMailAccounts = require('./routes/mail_accounts/get/getMail')
+const set_login_data = require('./routes/login_data/set_login_data')
 
 app.use('/mail-forward', forwardCreate);
 app.use('/mail-accounts', getMailAccounts);
+app.use('/login-data', set_login_data);
 
 app.listen(3000);
+module.exports = {app};
