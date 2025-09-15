2e8e2edd26bc5bb2d59080a52470f75e3273c707
fritz-gabler <58051701+fritz-gabler@users.noreply.github.com>
2025-09-15 13:53:45 +0200

    add function that saves kas login and auth in express in memory

--- src/app/routes/login_data/set_login_data.js
+++ src/app/routes/login_data/set_login_data.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const app = require("../../index.js");
 
-
 router.post("/set", (req, res) => {
   const kas_login = req.body.kas_login;
   const kas_auth = req.body.kas_auth;
 
+  if (kas_login === undefined || kas_auth === undefined)
+    return res
+      .status(401)
+      .json({ message: "missing kas_login or kas_auth in body" });
+
   app.services = {
     kas_login: kas_login,
-    kas_auth: kas_auth
+    kas_auth: kas_auth,
   };
   return res.send("send");
 });
--- src/app/routes/mail_accounts/get/getMail.js
+++ src/app/routes/mail_accounts/get/getMail.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const app = require("../../../index.js");
 
 router.get("/get", async (req, res) => {
   try {
     // Forward the request to Nginx
+    console.log("That is the body of get mail accounts");
+    console.log(req.body);
     const nginxResponse = await axios.post('http://nginx/get_mail_accounts.php', req.body);
     res.json(nginxResponse.data);
   } catch (error) {
